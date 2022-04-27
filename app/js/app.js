35234360b262afbdd7001ac2c96d5a66ebd93303
Ahmed102952 <ahmeda8400@gmail.com>
2022-04-27 19:20:01 +0200

    f

---
+++ app/js/app.js
@@ -0,0 +1,74 @@
+const inputs = document.querySelectorAll('input');
+const billInput = document.querySelector('#bill');
+const tipBtnInput = document.querySelectorAll('#tipBtn');
+const customTipBtnInput = document.querySelector('#customTipBtn');
+const peoNumInput = document.querySelector('#peopleNum');
+const tipAmountDisplay = document.querySelector('#tipAmount');
+const totalDisplay = document.querySelector('#total');
+const resetBtn = document.querySelector('#reset');
+let tipPercent = 0;
+let tipAmount = 0;
+let total = 0;
+let bill = 0;
+let peoNum = 0;
+
+tipBtnInput.forEach(e => {
+    e.addEventListener('click', (event) => {
+        event.preventDefault();
+        tipPercent = e.value;
+        checkInputs();
+        tipBtnInput.forEach(element=> {element.classList.remove('active')})
+        e.classList.add('active');
+    })
+})
+
+customTipBtnInput.addEventListener('input', () => {
+    tipBtnInput.forEach(element=> {element.classList.remove('active')})
+    tipPercent = customTipBtnInput.value;
+})
+
+inputs.forEach(e => {
+    e.addEventListener('input', () => {
+        bill = billInput.value;
+        peoNum = peoNumInput.value;
+
+        tipAmount = (bill * (tipPercent / 100)) / peoNum;
+        total = (bill / peoNum) + tipAmount;
+
+        checkInputs()
+
+    })
+})
+resetBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    reset();  
+})
+
+
+
+const checkInputs = () => {
+        resetBtn.classList.remove('disabled');
+        if(tipAmount) {
+            tipAmountDisplay.innerHTML = tipAmount.toFixed();
+            totalDisplay.innerHTML = total.toFixed();
+        }
+}
+
+const reset = () => {
+    billInput.value = ''
+    customTipBtnInput.value = ''
+    peoNumInput.value = ''
+    tipBtnInput.forEach(element=> {element.classList.remove('active')})
+
+    tipPercent = 0;
+    tipAmount = 0;
+    total = 0;
+    bill = 0;
+    peoNum = 0;
+    resetBtn.classList.add('disabled');
+
+    tipAmountDisplay.innerHTML = '0';
+    totalDisplay.innerHTML = '0';
+
+}
+reset();
